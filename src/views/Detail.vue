66f999279bf3b853857ae11e5b4dbd9936bfbcd6
xuejiuainiyo <3068440939@qq.com>
2023-06-19 19:13:16 +0800

    首页完成及商品的详细页面的数据请求

---
+++ src/views/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    详情页
+  </div>
+</template>
+
+<script>
+import {GoodsDetailsAPI} from "@/request/api"
+export default {
+    async created(){
+        let id = this.$route.query.id;
+        let res = await GoodsDetailsAPI(id);
+        console.log("商品详细页面","id="+ id,res);
+    }
+}
+</script>
+
+<style>
+
+</style>
